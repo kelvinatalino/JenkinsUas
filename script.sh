3bafc9ff884192015b097388954c0d6cabf83712
user <user@localhost.localdomain>
2021-02-19 00:45:53 +0700

    Script Updated

--- script.sh
+++ script.sh
@@ -1,11 +1,1 @@
-# Stop all running containers
-docker stop $(docker ps -aq)
-# Remove all containers
-docker rm $(docker ps -aq)
-
-# Remove php-apache image
-docker rmi trucorp-web-2.0
-# Remove mysql image
-docker rmi trucorp-web-2.0-mysql
-
-echo "---Successfully Remove Containers and Images"
+echo "---Successfully Running Jenkins"
